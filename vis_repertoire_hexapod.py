d3471980a20e05172f68357a45eadbf30abd3664
Luca Grillotti <luca.grillotti16@imperial.ac.uk>
2021-05-20 14:26:45 +0100

    adding correct axis representation for vis_repertoire

--- vis_repertoire_hexapod.py
+++ vis_repertoire_hexapod.py
@@ -1,16 +1,11 @@
-import sys, os
 import argparse
+import sys
 
-import pandas as pd
-import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib
 import matplotlib.pyplot as plt
+import pandas as pd
 
-import time
 import src.io as io
 from hexapod_controller import Hexapod
-
 
 ## Example to task Hexapod class ##
 ports = io.get_available_ports()
@@ -24,6 +19,7 @@ print('Using the first on the list', port)
 ctrl_freq = 100
 Hexa = Hexapod(port, ctrl_freq)
 Hexa.neutral_controller()
+
 
 def key_event(event, args):
     if event.key == 'escape':
@@ -42,27 +38,27 @@ def click_event(event, args):
     selected_x = selected_cell[0]
     selected_y = selected_cell[1]
     '''
-    #event.button ==1 is the left mouse click
+    # event.button ==1 is the left mouse click
     if event.button == 1:
         selected_x = int(event.xdata)
         selected_y = int(event.ydata)
-        selected_solution = data[(data["y_bin"] == selected_x) & (data["x_bin"] == selected_y)]
-        #selected_solution = data[(data["y_bin"] == selected_x) & (data["z_bin"] == selected_y)]
+        selected_solution = data[(data["x_bin"] == selected_x) & (data["y_bin"] == selected_y)]
+        # selected_solution = data[(data["y_bin"] == selected_x) & (data["z_bin"] == selected_y)]
 
         # For hexapod omnitask
-        print("SELECTED SOLUTION SHAPE: ",selected_solution.shape)
+        print("SELECTED SOLUTION SHAPE: ", selected_solution.shape)
         selected_solution = selected_solution.iloc[0, :]
         selected_ctrl = selected_solution.iloc[4:-2].to_numpy()
-        #print(selected_ctrl[0].shape) #(1,36)
+        # print(selected_ctrl[0].shape) #(1,36)
 
-        #hexapod uni
-        #selected_solution = selected_solution.iloc[0, :]
-        #selected_ctrl = selected_solution.iloc[8:-2].to_numpy()
-        
-        #print("Selected ctrl shape: ", selected_ctrl.shape) # should be 3661
-        #print("Selected descriptor bin: " ,selected_x, selected_y)
+        # hexapod uni
+        # selected_solution = selected_solution.iloc[0, :]
+        # selected_ctrl = selected_solution.iloc[8:-2].to_numpy()
+
+        # print("Selected ctrl shape: ", selected_ctrl.shape) # should be 3661
+        # print("Selected descriptor bin: " ,selected_x, selected_y)
         print("Selected descriptor from archive: ", selected_solution.iloc[1:3].to_numpy())
-        #print("Selected fitness from archive: ", selected_solution.iloc[0])
+        # print("Selected fitness from archive: ", selected_solution.iloc[0])
 
         # ---- PLAY THE SELECTED CONTROLLER -----#        
         Hexa.run_sin_controller(selected_ctrl, duration=3.0)
@@ -70,63 +66,63 @@ def click_event(event, args):
         print("EXECUTE CONTROLLER")
 
 
-    
+def read_archive_luca(filename):
+    data = pd.read_csv(filename, delim_whitespace=True)
+    # data = data.iloc[:,:-1] # drop the last column which was made because there is a comma after last value i a line
+    # data = np.loadtxt(args.filename)
 
+    # exchanging x & y axis + inverting left and right.
+    data["scale_x"] = (-1 * data.iloc[:, 2] + 1.2) / 3
+    data["scale_y"] = (data.iloc[:, 1] + 1.2) / 3
+
+    # For Hexapod
+    data['x_bin'] = pd.cut(x=data["scale_x"],
+                           bins=[p / 100 for p in range(101)],
+                           labels=[p for p in range(100)])
+    data['y_bin'] = pd.cut(x=data["scale_y"],
+                           bins=[p / 100 for p in range(101)],
+                           labels=[p for p in range(100)])
+
+    return data
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--filename", type=str) # file to visualize rollouts from
+    parser.add_argument("--filename", type=str)  # file to visualize rollouts from
     parser.add_argument("--plot_type", type=str, default="grid", help="scatter plot or grid plot")
 
     args = parser.parse_args()
-    
-    data = pd.read_csv(args.filename, delim_whitespace=True)
-    # data = data.iloc[:,:-1] # drop the last column which was made because there is a comma after last value i a line
-    #data = np.loadtxt(args.filename)
-    
-        
-    data["scale_x"] = (data.iloc[:,1] + 1.5) / 3
-    data["scale_y"] = (data.iloc[:,2] + 1.5) / 3
 
-    #For Hexapod
-    data['x_bin']=pd.cut(x = data["scale_x"],
-                        bins = [p/100 for p in range(101)], 
-                        labels = [p for p in range(100)])
-    data['y_bin']=pd.cut(x = data["scale_y"],
-                        bins = [p/100 for p in range(101)],
-                        labels = [p for p in range(100)])
+    data = read_archive_luca(args.filename)
 
-    #cmap = matplotlib.cm.get_cmap('Spectral') # Getting a list of color values
-    #data['color_dict'] = pd.Series({k:cmap(1) for k in data['scaled_x']})
-    
-    #=====================PLOT DATA===========================#
+    # cmap = matplotlib.cm.get_cmap('Spectral') # Getting a list of color values
+    # data['color_dict'] = pd.Series({k:cmap(1) for k in data['scaled_x']})
+
+    # =====================PLOT DATA===========================#
 
     # FOR BINS / GRID
     if args.plot_type == "grid":
         fig, ax = plt.subplots()
-        data.plot.scatter(x="y_bin",y="x_bin",c=3,colormap="viridis", s=2, ax=ax)
-        plt.xlim(0,100)
-        plt.ylim(0,100)    
+        data.plot.scatter(x="x_bin", y="y_bin", c=3, colormap="viridis", s=2, ax=ax)
+        plt.xlim(0, 100)
+        plt.ylim(0, 100)
     else:
-        #fig, ax = plt.subplots(nrows=1, ncols=2)
+        # fig, ax = plt.subplots(nrows=1, ncols=2)
         fig, ax = plt.subplots()
 
         # FOR JUST A SCATTER PLOT OF THE DESCRIPTORS - doesnt work for interactive selection
-        #data.plot.scatter(x=2,y=3,c=0,colormap='Spectral', s=2, ax=ax, vmin=-0.1, vmax=1.2)
-        data.plot.scatter(x=1,y=2,c=0,colormap='viridis', s=2, ax=ax)
+        # data.plot.scatter(x=2,y=3,c=0,colormap='Spectral', s=2, ax=ax, vmin=-0.1, vmax=1.2)
+        data.plot.scatter(x=1, y=2, c=0, colormap='viridis', s=2, ax=ax)
 
-        #data.plot.scatter(x=1,y=2,s=2, ax=ax[0])
-        #data.plot.scatter(x=3,y=4,c=0,colormap='viridis', s=2, ax=ax)
-        #data.plot.scatter(x=4,y=5,s=2, ax=ax[1])
-        #plt.xlim(-0.5,0.5)
-        #plt.ylim(-0.5,0.5)
-        
+        # data.plot.scatter(x=1,y=2,s=2, ax=ax[0])
+        # data.plot.scatter(x=3,y=4,c=0,colormap='viridis', s=2, ax=ax)
+        # data.plot.scatter(x=4,y=5,s=2, ax=ax[1])
+        # plt.xlim(-0.5,0.5)
+        # plt.ylim(-0.5,0.5)
+
     # event to look out. visualization or closing the plot
     fig.canvas.mpl_connect('key_press_event', lambda event: key_event(event, args))
     fig.canvas.mpl_connect('button_press_event', lambda event: click_event(event, args))
-    
-    plt.show() 
 
-    
+    plt.show()
